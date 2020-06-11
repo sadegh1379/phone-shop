1f2e3be510a70fe7eaa844c075a162ead46ebf22
sadegh <akbarisadegh382@gmail.com>
2020-06-11 22:44:56 +0430

    color

---
+++ .history/src/Context_20200611221516.js
@@ -0,0 +1,326 @@
+import React, { Component } from 'react';
+import {storeProducts , DetailProduct} from './data';
+
+const productContext = React.createContext();
+
+ class ProductProvider extends Component {
+    
+     
+
+         state ={
+             products: [] ,
+             detail : DetailProduct ,
+             cart : [] ,
+             openModal : false ,
+             closeModal : false ,
+             cartSubTotal : 0 ,
+             cartTax : 0 ,
+             total : 0 ,
+             counterCart :0,
+             loading : false
+
+
+         }
+
+    // set products in state************************************
+
+     componentDidMount(){
+        // this.startSaveInLocalStorage();
+        this.handleset()
+        window.localStorage.setItem("products" , JSON.stringify(storeProducts));
+
+        
+        
+    }
+    
+     componentWillUpdate(){
+            this.startSaveInLocalStorage();
+    }
+
+      // local Storage*****************************************
+
+      setStorage(value){
+        return window.localStorage.getItem(value) ? JSON.parse(window.localStorage.getItem(value)):[];
+
+        }
+
+    startSaveInLocalStorage = ()=>{
+
+        const { products , detail , cartSubTotal , cartTax , total ,counterCart , cart } = this.state;
+            
+            window.localStorage.setItem("products" , JSON.stringify(products));
+            window.localStorage.setItem("cart" , JSON.stringify(cart));
+            window.localStorage.setItem("cartSubTotal" , JSON.stringify(cartSubTotal));
+            window.localStorage.setItem("cartTax" , JSON.stringify(cartTax));
+            window.localStorage.setItem("total" , JSON.stringify(total));
+            window.localStorage.setItem("counterCart" , JSON.stringify(counterCart));
+            
+
+
+    }
+
+
+    // handle set************************************************
+    handleset =  () =>{
+       let tempProducts = []
+    
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts , singleItem];
+        }
+         )
+
+         this.setState(()=>{
+             return{
+                 products : tempProducts
+             }
+         })
+
+         
+    }
+
+    // set total************************************
+
+    setTotal = ()=>{
+        let subTotal = 0 ;
+        this.state.cart.map((item)=>{
+            subTotal += Number(item.total)
+        })
+
+        const temTax = subTotal * 0.01 ;
+        const tax = parseFloat(temTax.toFixed(2))
+        const total = tax + subTotal
+
+        this.setState(()=>{
+            return{
+                total : total ,
+                tax : tax ,
+                subTotal : subTotal
+            }
+        })
+    }
+
+    
+
+    // handle Modal************************************
+
+    handleDetail = (id)=>{
+        
+        let tempProduct = [...this.state.products];
+        const index= tempProduct.indexOf(this.findProduct(id));
+        const product = tempProduct[index];
+
+        this.setState(()=>{
+            return{
+                detail:product ,
+                openModal : true
+            }
+        })
+
+    }
+
+   
+
+  
+    // find product********************************************
+
+    findProduct = (id)=>{
+       const product =  this.state.products.find(item => item.id === id);
+       return product;
+    }
+
+    // add to cart*********************************************
+
+    addToCart = (id)=>{
+        let tempProduct = [...this.state.products];
+        const index = tempProduct.indexOf(this.findProduct(id));
+        const product = tempProduct[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price ;
+        product.total = price ;
+        
+
+        this.setState(()=>{
+            return{
+                products : tempProduct ,
+                cart : [...this.state.cart , product] ,
+                counterCart : Number(this.state.counterCart) + 1
+            }
+        },()=>{
+            this.setTotal();
+        }
+        )
+
+    }
+
+
+    // remove item cart*****************************************
+
+    handleRemoveItem = (id)=>{
+        let tempProduct = [...this.state.products];
+        const index = tempProduct.indexOf(this.findProduct(id));
+        const product = tempProduct[index];
+        product.inCart = false;
+        product.total = 0 ;
+
+        
+        this.setState(()=>{
+            return{
+                products : tempProduct ,
+                cart : this.state.cart.filter((item)=> item.id != id),
+                counterCart : this.state.counterCart - 1 
+                
+            }
+        } , ()=> this.setTotal())
+
+       
+    }
+
+    // clear cart*******************************************
+
+    clearCart = ()=>{
+        let q = window.confirm("آیا میخواهید کل لیست را حذف کنید ؟");
+        
+        if(q == true){
+            let temproduct = [...this.state.products];
+             temproduct.map(item=>{
+            item.inCart = false
+        });
+       
+            this.setState(()=>{
+                return{
+                    products : temproduct,
+                    cart : [] ,
+                    counterCart : 0
+                }
+            } , ()=>this.setTotal());
+        }
+        
+    }
+
+    // decreament**********************************************
+
+    decreament = (id)=>{
+            let temp = [...this.state.cart];
+            const index = temp.indexOf(this.findProduct(id));
+            const product = temp[index];
+            if(product.count === 1){
+                let q = window.confirm('آیا میخواهید این کالا را حذف کنید');
+                if(q==true){
+                    this.handleRemoveItem(id)
+                }else{
+                    return;
+                }
+                
+            }else{
+                product.count = product.count - 1
+                product.total = product.price * product.count ;
+
+
+                this.setState(()=>{
+                   return{
+                    cart : temp ,
+                    
+                   } 
+                 } , ()=>this.setTotal())
+            }
+        
+    }
+
+    // increament**********************************************
+
+    increament = (id)=>{
+        let temp = [...this.state.cart];
+        const index = temp.indexOf(this.findProduct(id));
+        const product = temp[index];
+        product.count = product.count + 1
+        product.total = product.price * product.count ;
+
+        this.setState(()=>{
+           return{
+            cart : temp , 
+            
+           } 
+        } , ()=>this.setTotal())
+    }
+
+    // add comments 
+
+    addComment = (name , comment , id)=>{
+       
+    
+            let tempProduct = [...this.state.products];
+            const index = tempProduct.indexOf(this.findProduct(id));
+            const product = tempProduct[index];
+            let newComment = {
+                name : name ,
+                text : comment
+            }
+      
+    
+            product.comments = [...product.comments , newComment]
+    
+            this.setState(()=>{
+                return{
+                    products : tempProduct
+                }
+            })
+        
+        
+        
+
+    }
+
+
+    // change Phone Color Price 
+
+    changeColor = (id , color) =>{
+        let tempProduct = [...this.state.products];
+        const index = tempProduct.indexOf(this.findProduct(id));
+        const product = tempProduct[index];
+
+        switch (color) {
+            case 'red':
+                product.main_color = product.red_price
+            case 'gold': 
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+     
+
+
+    render() {
+            
+       
+        
+        return (
+            <productContext.Provider value={{
+                                    ...this.state ,
+                                    addToCart : this.addToCart,
+                                    handleDetail : this.handleDetail,
+                                    handleRemoveItem : this.handleRemoveItem,
+                                    decreament : this.decreament,
+                                    increament : this.increament,
+                                    clearCart : this.clearCart,
+                                    openLoading : this.openLoading,
+                                    addComment : this.addComment
+                                    
+
+                                }}>
+                                                            {
+                                                                this.props.children
+                                                            }
+            </productContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = productContext.Consumer;
+
+export { ProductProvider , ProductConsumer} ;
+
---
+++ .history/src/component/Detail_20200611224220.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import { ProductConsumer } from '../Context';
+import {Link } from 'react-router-dom'
+
+export default class Detail extends Component {
+
+    state={
+        name : '',
+        text :'',
+        open : false
+    }
+
+    handleOpen = ()=>{
+        this.setState({
+            open : !this.state.open
+        })
+
+        
+    }
+
+
+    handleChangeName = (e)=>{
+        this.setState({
+                name : e.target.value
+            }
+        )
+    }
+    handleChangeText = (e)=>{
+        this.setState(
+            {
+                text : e.target.value
+            }
+        )
+    }
+
+    render() {
+        return (
+            <ProductConsumer>
+                {(value)=>{
+                    const {id , title , img , price , company ,color_e , color_p  , comments, info , inCart } = value.detail;
+                    const {addToCart  , addComment , changeColor} = value;
+                    return(
+                        <>
+                        <div className="row text-center d-flex   my-0 py-1 " >
+                               
+                                <div className="col-lg-6 text-right col-md-6 my-md-3 justify-content-between align-items-center my-sm-3  col-sm-10 py-4 px-5">
+                                        <h3 className="text-dark text-center wow bounceInRight" data-wow-dely="3s" data-wow-duration="2s">{title}</h3><hr className="text-light  my-4"/>
+
+                                        <h6 data-wow-dely="3s" data-wow-duration="2s" className="wow bounceInRight">
+                                                <span className="text-muted "> قیمت : </span><span className="text-danger cost float-left">{price} تومان</span>
+                                        </h6>
+
+                                        <h6 className="my-3" data-wow-dely="3s" data-wow-duration="2s" className="wow bounceInRight">
+                                        <span className="text-muted"> شرکت سازنده : </span><span className=" float-left h5">{company} </span>
+                                        </h6> 
+
+                                        <h6 className="wow bounceInRight"  data-wow-dely="3s" data-wow-duration="2s">
+                                        <span className="text-muted">  رنگ : 
+                                        </span>
+                                             <span className=" float-left floatspan">
+                                        
+                                                <span className="badge " style={{ backgroundColor: color_e , color: color_e}}>C</span>
+                                                {color_p} 
+                                        
+                                            </span>
+                                        </h6> 
+                                        <h6 className="wow bounceInRight"  data-wow-dely="3s" data-wow-duration="2s">
+
+                                        <span className="text-muted">  رنگ بندی : 
+                                        </span>
+                                             <span className=" float-left floatspan  ml-2">
+                                                <span onClick={} className="btn btn-sm" style={{ backgroundColor: 'red' , color: 'red'}}>C</span>
+                                                <span className="btn btn-sm mx-1" style={{ backgroundColor: 'gold' , color: 'gold'}}>C</span>
+                                                <span className="btn btn-sm " style={{ backgroundColor: 'black' , color: 'black'}}>C</span>
+
+                                            </span>
+                                        </h6> 
+
+
+                                        <h6 data-wow-dely="1s" data-wow-duration="2s" className="wow bounceInUp">
+                                        <span className="text-muted">  خصوصیات : </span><span className=" float-left my-3">{info} </span>
+                                        </h6> 
+
+                                        
+
+                                        <div className=" text-center my-4">
+                                           
+                                            <button disabled={inCart?true : false}
+                                            
+                                            onClick={()=>addToCart(id)}
+                                            className="btn float-right btn-outline-info flat">
+                                                    {inCart?    (<div>
+                                                                  <span className="fa fa-shopping-basket"></span><span>در سبد وجود دارد</span>
+                                                                </div>)
+                                                         : '  افزودن به سبد  '
+                                                        
+                                                }
+                                            </button>
+                                            <Link to="/" className="btn btn-outline-success float-left">
+                                                بازگشت <span className="fa fa-arrow-left"></span>
+                                            </Link>
+
+                                        </div>
+                                        
+                                        
+                                </div>
+                                
+                                <div className="col-lg-6 col-sm-10 col-md-6 p-4">
+                                        <img style={{width:'80%' }} src={img} alt={title}/>
+                                </div>
+                        </div>
+                        <div className="row  p-3 mx-auto text-center">
+                     
+                                            
+                                                <ul className="list-group comments-ul list-group-flush py-3 list-unstyled cost transition " style={{width:'100%'}}>
+                                                <h6 className="text-muted "
+                                                 onClick={this.handleOpen}
+                                                 style={{cursor:'pointer'}}
+                                                 >نظر کاربران <span
+                                               
+                                                className={`fa  fa-chevron-circle-${this.state.open ? 'up' : 'down'}   `} style={{cursor:'pointer'}}></span></h6>
+                                                
+                                                { 
+                                                    comments && this.state.open   ? comments.map((com , index)=>{
+                                                        return <li key={index} className="list-group-item list-group-item-dark py-3  comments  px-1 text-right"
+                                                        
+                                                        >
+                                                                    <h6 className="float-right  badge badge-warning">{com.name}</h6><br/>
+                                                                    <p className="float-center text-muted">{com.text}</p>
+                                                            </li>
+                                                    }) : null
+
+                                                    
+                                                }
+                                                <li>
+                                                <form 
+                                                onSubmit={(e)=>{
+                                                    e.preventDefault()
+                                                    const {name , text} = this.state;
+                                                    if(name === '' || text === ''){
+                                                        alert('لطفا هر دو فیلد را پر کنید')
+                                                    
+                                                    }else{
+                                                       
+                                                        addComment(name , text , id);
+                                                        this.state.name = ''
+                                                        this.state.text = ''
+                                                    }
+                                                   
+                                                }}>
+                                                        <div className="row mt-3 px-2 mx-auto">
+                                                            <div className="col">
+                                                            <input type="text"  className="form-control form-control-sm" placeholder=" نام"
+                                                            value={this.state.name}
+                                                            name="name"
+                                                            onChange={(e)=>this.handleChangeName(e)}
+                                                            />
+                                                            </div>
+                                                            <div className="col">
+                                                            <input type="text" className="form-control form-control-sm" placeholder="نظر شما"
+                                                            value={this.state.text}
+                                                            name="text"
+                                                            onChange={(e)=>this.handleChangeText(e)}
+                                                            />
+                                                            </div>
+                                                        </div>
+                                                        <button type="submit" className="btn  btn-dark btn-block mt-4">افزودن نظر</button>
+                                                </form>
+                                                   
+                                                </li>
+                                                </ul>
+                                           
+                                            
+                                        
+                            
+                        </div>
+                    </>
+                    )
+                }
+
+                }
+            </ProductConsumer>
+        )
+    }
+}
